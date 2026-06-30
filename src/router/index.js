15bed25357b35e9fc654320fb4c2d088881137f5
jamie <jamiechang1001@gmail.com>
2026-06-30 18:42:03 +0800

    修复 GitHub Pages 路由问题：使用 createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   {
@@ -32,7 +32,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
